a93709a9d04e539d465186c4f3d66edac8947776
Mateusz Kocz <mateusz@kocz.pl>
2019-03-09 20:43:47 +0100

    Success page also deserves a link

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 const Success = () => (
   <Layout title="Message sent">
     <h1>Message sent successfully.</h1>
     <p>Thanks! Be in touch shortly.</p>
-    <p>Meanwhile, <a href="/">go back to the home page</a>.</p>
+    <p>Meanwhile, <Link to="/">go back to the home page</Link>.</p>
   </Layout>
 )
 
