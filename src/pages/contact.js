edc89879780f7ece74794271e15a6d605ce17cc9
Mateusz Kocz <mateusz@kocz.pl>
2019-03-09 18:25:24 +0100

    Style the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,51 @@
 import React from "react"
+import styled, {css} from 'styled-components'
 import Layout from "../components/layout"
+
+const Label = styled.label`
+  span {
+    display: block;
+  }
+`
+
+const inputsStyles = css`
+  display: block;
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #a1a1a1;
+  padding: .5rem 0;
+  font-size: 1rem;
+  
+  &:invalid, &:focus {
+    outline: none;
+  }
+  
+  &:focus {
+    border-bottom-color: black;
+  }
+`
+
+const Input = styled.input`
+  ${inputsStyles};
+`
+
+const TextArea = styled.textarea`
+  ${inputsStyles};
+`
+
+const SubmitButton = styled.button`
+  margin-right: 1rem;
+  margin-bottom: 1.5rem;
+  border: 1px solid currentColor;
+  background: none;
+  font-size: inherit;
+  padding: 1rem;
+  line-height: 1;
+`
 
 const Contact = () => (
   <Layout title="Contact">
+    <h1>Contact</h1>
     <form
       name="contact-form"
       method="post"
@@ -11,19 +54,28 @@ const Contact = () => (
       data-netlify-honeypot="bot-field"
     >
       <input type="hidden" name="bot-field"/>
-      <label>
-        Your Name:
-        <input type="text" name="name" required/>
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" required/>
-      </label>
-      <label>
-        Message:
-        <textarea name="message" required/>
-      </label>
-      <button type="submit">Send</button>
+      <p>
+        <Label>
+          <span>Your name</span>
+          <Input type="text" name="name" required placeholder="Who are you?"/>
+        </Label>
+      </p>
+      <p>
+        <Label>
+          <span>Your email</span>
+          <Input type="email" name="email" required placeholder="How to reach you?"/>
+        </Label>
+      </p>
+      <p>
+        <Label>
+          <span>Message</span>
+          <TextArea name="message" required placeholder="What's up?" rows="5"/>
+        </Label>
+      </p>
+      <p>
+        <SubmitButton type="submit">Send</SubmitButton>
+        <a href="/">Back home</a>.
+      </p>
     </form>
   </Layout>
 )
