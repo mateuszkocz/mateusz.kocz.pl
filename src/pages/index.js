a66ae142d69b6f0c55e7e9517460d9e82b7d1f38
Mateusz Kocz <mateusz@kocz.pl>
2019-03-09 13:16:35 +0100

    Make the styling and positioning better

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,25 +14,31 @@ const GlobalStyles = createGlobalStyle`
 `
 
 const Main = styled.main`
-  position: relative;
   margin: 0 auto;
   padding: 1rem 3rem;
-  height: 100%;
+  min-height: 100vh;
   box-sizing: border-box;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
+
   font-size: 2rem;
   color: #444;
   
-  @media (min-width: 600px) {
+  @media (min-width: 34rem) {
+    padding: 1rem 0;
+    max-width: 80%;
+  }
+
+  @media (min-width: 50rem) {
     max-width: 60%;
   }
 
   h1 {
-    margin: 0;
+    margin: 1rem 0 0;
     font-size: inherit;
-    font-weight: normal;
+    font-weight: inherit;
   }
 
   p {
@@ -40,46 +46,53 @@ const Main = styled.main`
   }
 
   a {
-    color: inherit;
-    text-decoration: none;
     border-bottom: 1px solid;
     padding-bottom: 1px;
+    text-decoration: none;
+    color: inherit;
   }
 `
 
+const Content = styled.section`
+  margin: auto 0;
+`
+
 const Star = styled.aside`
-  position: absolute;
-  width: 100%;
-  bottom: 1rem;
-  text-align: center;
-  font-size: 1rem;
+  margin: 0 auto;
+  padding-top: 1rem;
+  font-size: 50%;
   color: #a1a1a1;
+
   span {
     cursor: pointer;
   }
 `
 
 const IndexPage = () => (
-  <Main>
+  <>
     <GlobalStyles/>
-    <h1>Hi, I'm Mateusz and I'm a software developer.</h1>
-    <p>
-      I create web applications in JavaScript and Typescript. I&nbsp;mostly use
-      React.
-      Sometimes I&nbsp;also write some Angular and server side Express code.
-    </p>
-    <p>
-      Check out my <a href="https://radiatingstar.com/">blog</a>,
-      {" "}<a href="https://github.com/mateuszkocz">code on GitHub</a>,
-      {" "}<a href="https://stackoverflow.com/users/1382409/mateusz-kocz">profile
-      on StackOverflow</a>
-      {" "}and a <a href="https://www.linkedin.com/in/mateuszkocz/">CV on
-      LinkedIn</a>.
-    </p>
-    <Star>
-      <span>★</span>
-    </Star>
-  </Main>
+    <Main>
+      <Content>
+        <h1>Hi, I'm Mateusz and I'm a software developer.</h1>
+        <p>
+          I create web applications in JavaScript and Typescript. I&nbsp;mostly
+          use React. Sometimes I&nbsp;also write some Angular and server-side
+          Express code.
+        </p>
+        <p>
+          Check out my <a href="https://radiatingstar.com/">blog</a>,
+          {" "}<a href="https://github.com/mateuszkocz">code on GitHub</a>,
+          {" "}<a href="https://stackoverflow.com/users/1382409/mateusz-kocz">
+          a&nbsp;profile on StackOverflow</a>
+          {" "}and a <a href="https://www.linkedin.com/in/mateuszkocz/">CV on
+          LinkedIn</a>.
+        </p>
+      </Content>
+      <Star>
+        <span>★</span>
+      </Star>
+    </Main>
+  </>
 )
 
 export default IndexPage
