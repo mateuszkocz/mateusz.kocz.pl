d12a44f269dcad5116df68093e5011680434930e
Mateusz Kocz <mateusz@kocz.pl>
2019-03-09 14:06:44 +0100

    Add the 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const NotFoundPage = () => (
-  <p>404</p>
+  <Layout>
+    <h1>Not Found.</h1>
+    <p>Yes, it's 404.</p><p>It's not what you're looking for. <a href="/">Go to the home page</a>.</p>
+  </Layout>
 )
 
 export default NotFoundPage
