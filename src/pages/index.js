3d4e1b174fc41d891b4ec6eddd47bd8ca0215da8
Mateusz Kocz <mateusz@kocz.pl>
2019-03-10 14:03:55 +0100

    Track outbound links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import Layout from "../components/layout"
 
 const IndexPage = () => (
@@ -11,12 +12,12 @@ const IndexPage = () => (
       Express code.
     </p>
     <p>
-      Check out my <a href="https://radiatingstar.com/">blog</a>,
-      {" "}<a href="https://github.com/mateuszkocz">code on GitHub</a>,
-      {" "}<a href="https://stackoverflow.com/users/1382409/mateusz-kocz">
-      a&nbsp;profile on StackOverflow</a>
-      {" "}and a <a href="https://www.linkedin.com/in/mateuszkocz/">CV on
-      LinkedIn</a>. Or <Link to="/contact">contact me</Link>.
+      Check out my <OutboundLink href="https://radiatingstar.com/">blog</OutboundLink>,
+      {" "}<OutboundLink href="https://github.com/mateuszkocz">code on GitHub</OutboundLink>,
+      {" "}<OutboundLink href="https://stackoverflow.com/users/1382409/mateusz-kocz">
+      a&nbsp;profile on StackOverflow</OutboundLink>
+      {" "}and a <OutboundLink href="https://www.linkedin.com/in/mateuszkocz/">CV on
+      LinkedIn</OutboundLink>. Or <Link to="/contact">contact me</Link>.
     </p>
   </Layout>
 )
