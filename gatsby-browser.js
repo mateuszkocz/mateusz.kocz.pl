43500349c8dd59a45fdf39272a291a3ba8a43c5e
Mateusz Kocz <mateusz@kocz.pl>
2021-03-07 21:17:54 +0100

    Set up Tailwind

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-import 'typeface-source-sans-pro'
+import "typeface-source-sans-pro"
+import "./src/styles/global.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@mateuszkocz`,
   },
   plugins: [
+    'gatsby-plugin-postcss',
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
