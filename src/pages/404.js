0c06547c6ce9b9d784c09e7d0efcf8cabd84c0d3
Mateusz Kocz <mateusz@kocz.pl>
2021-03-10 21:10:13 +0100

    Reorganize 404's content a bit

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,11 @@ import Layout from "../components/layout"
 
 const NotFoundPage = () => (
   <Layout title="404">
-    <h1 className="mt-4 mb-0">Not Found.</h1>
-    <p>
-      Yes, it's 404.
-    </p>
+    <h1 className="mt-4 mb-0">Not Found. Yes, it's 404.</h1>
     <p>
       It's not what you're looking for.
+    </p>
+    <p>
       <Link to="/">Go to the home page</Link>.
     </p>
   </Layout>
