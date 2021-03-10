5b03d81eb40532e24d2483ab56eccbd80c8cd7f9
Mateusz Kocz <mateusz@kocz.pl>
2021-03-10 21:24:01 +0100

    Fix the medium screen padding issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,10 @@ import { OutboundLink } from "gatsby-plugin-google-analytics"
 const Layout = ({ children, title = "" }) => (
   <>
     <SEO title={title} />
-    <main className="my-0 mx-auto min-h-screen sm:py-4 sm:px-0 md:px-12 py-4 px-12 flex flex-col justify-center align-middle">
+    <main className="my-0 mx-auto min-h-screen py-4 px-12 flex flex-col justify-center align-middle">
       <section className="content my-auto mx-auto text-4xl text-main leading-snug max-w-prose">{children}</section>
       <aside className="relative my-0 mx-auto pt-4 px-4 pb-0 text-sm text-main">
-        <aside className="absolute bottom-0 w-48 text-gray-400 right-full text-right">
+        <aside className="absolute bottom-0 w-48 text-gray-500 right-full text-right">
           <OutboundLink href="https://github.com/mateuszkocz/mateusz.kocz.pl">
             Source on GitHub
           </OutboundLink>
@@ -17,7 +17,7 @@ const Layout = ({ children, title = "" }) => (
         <Link to="/" className="border-none">
           ★
         </Link>
-        <aside className="absolute bottom-0 w-48 text-gray-400 left-full">
+        <aside className="absolute bottom-0 w-48 text-gray-500 left-full">
           Warsaw, {new Date().getFullYear()}
         </aside>
       </aside>
