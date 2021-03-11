b498264042855def87be97727127dd52a39bbc21
Mateusz Kocz <mateusz@kocz.pl>
2021-03-11 19:09:46 +0100

    Make the footer's positioning better in regard to the content

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,8 +6,8 @@ import { OutboundLink } from "gatsby-plugin-google-analytics"
 const Layout = ({ children, title = "" }) => (
   <>
     <SEO title={title} />
-    <main className="my-0 mx-auto min-h-screen py-4 px-12 flex flex-col justify-center align-middle">
-      <section className="content my-auto mx-auto text-4xl text-main leading-snug max-w-prose space-y-8">{children}</section>
+    <main className="my-0 mx-auto min-h-screen pb-12 pt-4 px-12 flex flex-col justify-center align-middle">
+      <section className="content my-auto mx-auto py-8 text-4xl text-main leading-snug max-w-prose space-y-8">{children}</section>
       <aside className="relative my-0 mx-auto pt-4 px-4 pb-0 text-sm text-main">
         <aside className="absolute bottom-0 w-48 text-gray-500 right-full text-right">
           <OutboundLink href="https://github.com/mateuszkocz/mateusz.kocz.pl">
