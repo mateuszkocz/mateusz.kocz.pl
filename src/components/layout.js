cbf583e7d8d357e5038fb34aa93cde04d31c8cef
Mateusz Kocz <mateusz@kocz.pl>
2021-03-13 16:11:43 +0100

    Move the light mode gradient to Tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,26 +3,36 @@ import { Link } from "gatsby"
 import SEO from "./seo"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-const Layout = ({ children, title = "" }) => (
-  <>
-    <SEO title={title} />
-    <main className="my-0 mx-auto min-h-screen pb-12 pt-4 px-12 flex flex-col justify-center align-middle">
-      <section className="content my-auto mx-auto py-8 text-4xl text-main leading-snug max-w-prose space-y-8">{children}</section>
-      <aside className="relative my-0 mx-auto pt-4 px-4 pb-0 text-sm text-main">
-        <aside className="absolute bottom-0 w-48 text-gray-500 right-full text-right">
-          <OutboundLink href="https://github.com/mateuszkocz/mateusz.kocz.pl">
-            Source on GitHub
-          </OutboundLink>
+const Layout = ({ children, title = "" }) => {
+  const sectionClasses = "my-auto mx-auto py-8 text-4xl text-main leading-snug " +
+    "max-w-prose space-y-8 bg-gradient-to-b from-main to-end bg-clip-text " +
+    "text-fill-transparent dark:bg-none dark:bg-clip-border dark:text-fill-current"
+  return (
+    <>
+      <SEO title={title}/>
+      <main
+        className="my-0 mx-auto min-h-screen pb-12 pt-4 px-12 flex flex-col justify-center align-middle">
+        <section
+          className={sectionClasses}
+        >{children}</section>
+        <aside
+          className="relative my-0 mx-auto pt-4 px-4 pb-0 text-sm text-main">
+          <aside
+            className="absolute bottom-0 w-48 text-gray-500 right-full text-right">
+            <OutboundLink href="https://github.com/mateuszkocz/mateusz.kocz.pl">
+              Source on GitHub
+            </OutboundLink>
+          </aside>
+          <Link to="/" className="border-none">
+            ★
+          </Link>
+          <aside className="absolute bottom-0 w-48 text-gray-500 left-full">
+            Warsaw, {new Date().getFullYear()}
+          </aside>
         </aside>
-        <Link to="/" className="border-none">
-          ★
-        </Link>
-        <aside className="absolute bottom-0 w-48 text-gray-500 left-full">
-          Warsaw, {new Date().getFullYear()}
-        </aside>
-      </aside>
-    </main>
-  </>
-)
+      </main>
+    </>
+  )
+}
 
 export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,33 @@
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        main: "#268bd2"
+        main: "#268bd2",
+        end: "#134669",
       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ["dark"]
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({addUtilities}) => {
+      addUtilities({
+        ".text-fill-transparent": {
+          "-webkit-text-fill-color": "transparent",
+        },
+        ".text-fill-current": {
+          "-webkit-text-fill-color": "currentColor",
+        }
+      }, {
+        variants: ["dark"]
+      })
+    })
+  ],
 }
